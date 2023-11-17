6e2f4ba7874c70c3d118dfefaef9d06539006d9a
Marcos <marcosmikolta@tutamail.com>
2023-11-17 12:04:50 +0100

    lista

--- .config/nvim/lua/mappin.lua
+++ .config/nvim/lua/mappin.lua
@@ -7,4 +7,5 @@ map('n', '<SPACE><SPACE>', '/(<>)<ENTER>4xi')
 
 -- LaTeX
 map('i', ';ex', '\\exists')
-map('i', ';fi', '\\begin{figure}<ENTER>\\centering<ENTER>\\includegraphics[width=(<>)\\textwidth]{img/(<>).png}<ENTER>\\caption{(<>)}<ENTER>\\label{(<>)}<ENTER><ESC>i\\end{figure}<ESC>5kB')
+map('i', ';fi', '\\begin{figure}[(<>)]<ENTER>\\centering<ENTER>\\includegraphics[width=(<>)\\textwidth]{img/(<>).png}<ENTER>\\caption{(<>)}<ENTER>\\label{(<>)}<ENTER><ESC>i\\end{figure}<ESC>5kB')
+map('i', ';li', '\\begin{itemize}<ENTER><ENTER>\\end{itemize}<ESC>ki<TAB>')
