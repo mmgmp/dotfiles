52e4387f93b4e541ae9dc7b0bd677dd6a8686cd5
Marcos <marcosmikolta@tutamail.com>
2023-11-17 12:39:12 +0100

    \item con espacio

--- .config/nvim/lua/mappin.lua
+++ .config/nvim/lua/mappin.lua
@@ -8,5 +8,5 @@ map('n', '<SPACE><SPACE>', '/(<>)<ENTER>4xi')
 -- LaTeX
 map('i', ';fi', '\\begin{figure}[(<>)]<ENTER>\\centering<ENTER>\\includegraphics[width=(<>)\\textwidth]{img/(<>).png}<ENTER>\\caption{(<>)}<ENTER>\\label{(<>)}<ENTER><ESC>i\\end{figure}<ESC>5kB')
 map('i', ';li', '\\begin{itemize}<ENTER>\\end{itemize}<ESC>kB')
-map('i', ';it', '\\item (<>)<ESC>')
+map('i', ';it', '\\item  (<>)<ESC>')
 map('i', ';se', '\\section{(<>)}<ESC>')
