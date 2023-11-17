929ea0d0bf9bffdedb25cb6090d3146771bc8359
Marcos <marcosmikolta@tutamail.com>
2023-11-17 12:36:38 +0100

    borrar exits

--- .config/nvim/lua/mappin.lua
+++ .config/nvim/lua/mappin.lua
@@ -6,6 +6,7 @@ end
 map('n', '<SPACE><SPACE>', '/(<>)<ENTER>4xi')
 
 -- LaTeX
-map('i', ';ex', '\\exists')
 map('i', ';fi', '\\begin{figure}[(<>)]<ENTER>\\centering<ENTER>\\includegraphics[width=(<>)\\textwidth]{img/(<>).png}<ENTER>\\caption{(<>)}<ENTER>\\label{(<>)}<ENTER><ESC>i\\end{figure}<ESC>5kB')
-map('i', ';li', '\\begin{itemize}<ENTER><ENTER>\\end{itemize}<ESC>ki<TAB>')
+map('i', ';li', '\\begin{itemize}<ENTER>\\end{itemize}<ESC>kB')
+map('i', ';it', '\\item (<>)<ESC>')
+map('i', ';se', '\\section{(<>)}<ESC>')
