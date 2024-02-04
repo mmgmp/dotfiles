d452527474badbad51ea02877dcbfbb943d2f4fe
Marcos <marcosmikolta@tutamail.com>
2024-02-04 20:37:38 +0100

    ;pk

--- .config/nvim/lua/mappin.lua
+++ .config/nvim/lua/mappin.lua
@@ -3,12 +3,13 @@ function map(m, k, v)
 end
 
 -- General
-map('i', '<SPACE><SPACE>', '<ESC>/<++><ENTER>4xi')
+map('n', '<SPACE><SPACE>', '<ESC>/<++><ENTER>4xi')
 
 -- LaTeX
-map('i', ';fi', '\\begin{figure}[<++>]<ENTER>\\centering<ENTER>\\includegraphics[width=<++>\\textwidth]{img/<++>.png}<ENTER>\\caption{<++>}<ENTER>\\label{<++>}<ENTER><ESC>i\\end{figure}<ESC>5kBi')
+map('i', ';fi', '\\begin{figure}[<++>]<ENTER>\\centering<ENTER>\\includegraphics[width=<++>\\textwidth]{img/<++>.png}<ENTER>\\caption{<++>}<ENTER>\\label{<++>}<ENTER>\\end{figure}<ESC>5kB')
 map('i', ';li', '\\begin{itemize}<ENTER>\\end{itemize}<ESC>ko<TAB>')
 map('i', ';it', '\\item<SPACE><ESC>A')
 map('i', ';se', '\\section{}<ESC>i')
 map('i', ';su', '\\subsection{}<ESC>i')
 map('i', ';ssu', '\\subsubsection{}<ESC>i')
+map('i', ';pk', '\\usepackage{}<ESC>i')
