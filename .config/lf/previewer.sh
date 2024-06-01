784d9757568c8d480c8edfc92d958244d7d62df5
mmgmp <marcosmikolta@tutamail.com>
2024-06-01 22:03:56 +0200

    preview zip

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-case "$(file -Lb --mime-type -- "$1")" in
-    image/*)
-        chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
-        exit 1
-        ;;
-    *)
-        echo "Not an image file, displaying as text"
-        cat "$1"
-        ;;
+case "$1" in
+	*.zip) unzip -l "$1";;
+	*) cat "$1";;
 esac
