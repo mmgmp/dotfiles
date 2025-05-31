3b0099322f03de6053fe1aa364e4bd5e9874e78b
mmgmp <marcosmikolta@tutamail.com>
2025-05-31 12:06:29 +0200

    no plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,4 @@
-local vim = vim
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
-vim.call('plug#end')
-
 -- Core
 require("core/config")
 require("core/keymap")
 require("core/filetype/tex")
-
--- Plugins
