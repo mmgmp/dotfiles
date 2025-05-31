625c5398de85f243bc1f7d1653b36830f9d76a0b
mmgmp <marcosmikolta@tutamail.com>
2025-05-31 12:05:23 +0200

    quitar compilador

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -8,9 +8,6 @@ vim.g.mapleader = ";"
 -- Atajos
 map('n', 'º', '@', { desc = "Activar macros" })
 
--- Compilador
-map('n', '<leader>c', ':! compiler %<CR>', { desc = "Compilar archivo dependiendo de la extensión" })
-
 -- Funciones
 map('n', '<F6>', ':setlocal spell! spelllang=es<CR>', { desc = "Activar/desactivar el corrector ortográfico" })
 
@@ -18,14 +15,9 @@ map('n', '<F6>', ':setlocal spell! spelllang=es<CR>', { desc = "Activar/desactiv
 map('n', '<Space><Space>', '/<++><CR>4xi', { desc = "Saltar al siguiente <++> e insertar" })
 map('n', 'n', 'nzz', { desc = "Centrar el texto al usar n" })
 map('n', 'N', 'Nzz', { desc = "Centrar el texto al usar N" })
-map('n', '<left>', '<cmd>echo "Usa h para moverte!!"<CR>')
-map('n', '<right>', '<cmd>echo "Usa l para moverte!!"<CR>')
-map('n', '<up>', '<cmd>echo "Usa k para moverte!!"<CR>')
-map('n', '<down>', '<cmd>echo "Usa j para moverte!!"<CR>')
 
 -- Cierres automáticos
 map('i', '(', '()<ESC>i', { desc = "(X) automáticas" })
 map('i', '{', '{}<ESC>i', { desc = "{X} automáticas" })
 map('i', '[', '[]<ESC>i', { desc = "[X] automáticas" })
 map('i', '<', '<><ESC>i', { desc = "<X> automáticas" })
-
