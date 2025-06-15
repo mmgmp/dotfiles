20517ca8c9005e0f9ea6dfc91cc39cc62ea1b284
mmgmp <marcosmikolta@tutamail.com>
2025-06-15 10:29:24 +0200

    swayhide newsboat mpv

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -10,6 +10,7 @@ map('n', 'º', '@', { desc = "Activar macros" })
 
 -- Funciones
 map('n', '<F6>', ':setlocal spell! spelllang=es<CR>', { desc = "Activar/desactivar el corrector ortográfico" })
+map('i', '<leader><', '<++><ESC>A', { desc = "<++> automáticos" })
 
 -- Navegación
 map('n', '<Space><Space>', '/<++><CR>4xi', { desc = "Saltar al siguiente <++> e insertar" })
