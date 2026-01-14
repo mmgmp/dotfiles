b00bbdf9016e6e5d83a859315c149ac6be01cd30
Marcos <103284514+mmgmp@users.noreply.github.com>
2026-01-14 10:35:38 +0100

    Add files via upload

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,16 +1,41 @@
-vim.opt.number = true -- Número de la linea
-vim.opt.relativenumber = true -- Números relativos de lineas
-vim.opt.cursorline = false -- Mostrar linea del cursor
-vim.opt.termguicolors = true -- Colores de la terminal
+-- Número de la linea
+vim.opt.number = true
 
-vim.opt.tabstop = 4 -- Número de espacios de tabulador
-vim.opt.shiftwidth = 4 -- Número de espacios automáticos
-vim.opt.softtabstop = 4 -- Número de espacios cuando se usa <BS> o <Del>
+-- Números relativos de lineas
+vim.opt.relativenumber = true
 
-vim.opt.mouse = "a" -- Activar el ratón
-vim.opt.clipboard = 'unnamedplus' -- Portapapeles del sistema
+-- Mostrar linea del cursor
+vim.opt.cursorline = false
 
-vim.opt.ttyfast = true -- Movimiento mas rápido
+-- Colores de la terminal
+vim.opt.termguicolors = false
 
-vim.o.ignorecase = true -- Buscar los mayúsculas
-vim.o.smartcase = true -- Solo mayúsculas si se especifica
+-- Número de espacios de tabulador
+vim.opt.tabstop = 4
+
+-- Número de espacios automáticos
+vim.opt.shiftwidth = 4
+
+-- Número de espacios cuando se usa <BS> o <Del>
+vim.opt.softtabstop = 4
+
+-- Activar el ratón
+vim.opt.mouse = "a"
+
+-- Portapapeles del sistema
+vim.opt.clipboard = 'unnamedplus'
+
+-- Mostrar columna de la izquierda
+vim.opt.signcolumn = 'yes'
+
+-- Movimiento mas rápido
+vim.opt.ttyfast = true
+
+-- Mostrar siempre 10 lineas por debajo del cursor
+vim.opt.scrolloff = 10
+
+-- Buscar los mayúsculas
+vim.o.ignorecase = true
+
+-- Solo mayúsculas si se especifica
+vim.o.smartcase = true
--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -8,14 +8,21 @@ vim.g.mapleader = ";"
 -- Atajos
 map('n', 'º', '@', { desc = "Activar macros" })
 
--- Funciones
+-- Activar corrector ortográfico
 map('n', '<F6>', ':setlocal spell! spelllang=es<CR>', { desc = "Activar/desactivar el corrector ortográfico" })
+
+-- Insertar <++> automáticos
 map('i', '<leader><', '<++><ESC>A', { desc = "<++> automáticos" })
 
--- Navegación
+-- Saltar al siguiente <++>
 map('n', '<Space><Space>', '/<++><CR>4xi', { desc = "Saltar al siguiente <++> e insertar" })
+
+-- Centrar texto al buscar palabra
 map('n', 'n', 'nzz', { desc = "Centrar el texto al usar n" })
 map('n', 'N', 'Nzz', { desc = "Centrar el texto al usar N" })
+
+-- Quitar texto resaltado
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Cierres automáticos
 map('i', '(', '()<ESC>i', { desc = "(X) automáticas" })
